461de89e9b957d2b31808e3362fbc4ce307c076b
Jaime Solà <74617905+jsola2@users.noreply.github.com>
2021-03-21 17:57:06 +0100

    Delete Taula Poblacions.sql

--- Sentencias SQL/Taula Poblacions.sql
+++
@@ -1,8 +0,0 @@
-USE db_hotels;
-
-ALTER TABLE poblacions
-
-	ADD COLUMN provincia_id SMALLINT UNSIGNED
-			AFTER poblacio_id,
-    ADD CONSTRAINT FK_poblacions_provincies FOREIGN KEY (provincia_id)
-			REFERENCES provincies (provincia_id);
